1172a1defa507088b9ff5e65348d359061a133c8
Artur <arturchih27@gmail.com>
2025-02-11 21:20:17 +0200

    update next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,3 +4,28 @@ module.exports = {
     generateRobotsTxt: true, // Создаст robots.txt автоматически
     generateIndexSitemap: false, // Отключаем sitemap-0.xml
   };
+
+  module.exports = {
+    async headers() {
+      return [
+        {
+          source: "/sitemap.xml",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "no-cache, no-store, must-revalidate",
+            },
+            {
+              key: "Pragma",
+              value: "no-cache",
+            },
+            {
+              key: "Expires",
+              value: "0",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
