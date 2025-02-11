b5aac1b9df1644212b1e3b7c16f7ddbdcc011e9b
Artur <arturchih27@gmail.com>
2025-02-11 21:09:06 +0200

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://sticks-front.vercel.app/', // ЗАМЕНИ НА СВОЙ ДОМЕН
-    generateRobotsTxt: true, // Автоматически создаст robots.txt
-    changefreq: 'daily', // Как часто обновляются страницы
-    priority: 0.7, // Приоритет страниц (0.1 - 1.0)
+    siteUrl: 'https://sticks-front.vercel.app', // URL твоего сайта
+    generateRobotsTxt: true, // Создаст robots.txt автоматически
+    generateIndexSitemap: false, // Отключаем sitemap-0.xml
   };
