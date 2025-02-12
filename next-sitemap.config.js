ede5429025c764f897d8f95be39dec54ccbfc4f3
Artur <arturchih27@gmail.com>
2025-02-12 19:32:46 +0200

    update sidemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,28 +1,36 @@
 /** @type {import('next-sitemap').IConfig} */
-  module.exports = {
-    siteUrl: 'https://sticks-front.vercel.app', // URL твоего сайта
-    generateRobotsTxt: true, // Создаст robots.txt автоматически
-    generateIndexSitemap: false, // Отключаем sitemap-0.xml
-    async headers() {
-      return [
-        {
-          source: "/sitemap.xml",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "no-cache, no-store, must-revalidate",
-            },
-            {
-              key: "Pragma",
-              value: "no-cache",
-            },
-            {
-              key: "Expires",
-              value: "0",
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
+module.exports = {
+  siteUrl: 'https://sticks-front.vercel.app',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: ['/', '/ru', '/en', '/game/'],
+
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+    ];
+  },
+};
